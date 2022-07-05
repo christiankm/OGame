1220847787d36fbd0e1b23d9c23d8f32d8434883
Christian Mitteldorf <christian.km@me.com>
2022-07-06 03:33:19 +1000

    Fix build errors and add test for Diameter

--- Tests/OGameTests/OGameTests.swift
+++ Tests/OGameTests/OGameTests.swift
@@ -1,11 +1,10 @@
-    import XCTest
-    @testable import Ogame
+import XCTest
+@testable import OGame
 
-    final class OgameTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(Ogame().text, "Hello, World!")
-        }
+final class DiameterTests: XCTestCase {
+    
+    func testDiameterIsInt() {
+        let sut: Diameter = 8888
+        XCTAssertEqual(sut, Int(8888))
     }
+}
