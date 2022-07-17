9832e11b74cc4af53e29fd1640680a1576ca3a22
Christian Mitteldorf <christian@mitteldorf.dk>
2022-07-17 17:35:51 +1000

    Conform tradeship to Identifiable

--- Sources/OGame/Ships/TradeShip.swift
+++ Sources/OGame/Ships/TradeShip.swift
@@ -1,13 +1,14 @@
 //
-//  File.swift
-//
-//
-//  Created by Christian Mitteldorf on 12/09/2021.
+//  OGame
+//  Copyright © 2022 Christian Mitteldorf. All rights reserved.
+//  MIT license, see LICENSE file for details.
 //
 
 import Foundation
 
 public struct TradeShip: Ship {
+    public let id = 0
     public var name: String = "Trade Ship"
 
+    public init() {}
 }
