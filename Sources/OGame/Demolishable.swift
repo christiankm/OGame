b850f7b06168db4bea8a0d6550e84a0a4bd7cd1b
Christian Mitteldorf <christian@mitteldorf.dk>
2022-07-20 22:03:54 +1000

    Add protocol for Buildings

---
+++ Sources/OGame/Demolishable.swift
@@ -0,0 +1,10 @@
+//
+//  OGame
+//  Copyright © 2022 Christian Mitteldorf. All rights reserved.
+//  MIT license, see LICENSE file for details.
+//
+
+import Foundation
+
+public protocol Demolishable {
+}
