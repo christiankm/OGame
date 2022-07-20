d4d6d98010f61c79b4532fc31e2be4ddb6eda165
Christian Mitteldorf <christian@mitteldorf.dk>
2022-07-20 14:42:07 +1000

    Add Unit protocol
    Format code

---
+++ Sources/OGame/Unit.swift
@@ -0,0 +1,11 @@
+//
+//  OGame
+//  Copyright © 2022 Christian Mitteldorf. All rights reserved.
+//  MIT license, see LICENSE file for details.
+//
+
+import Foundation
+
+/// Units describe military units (ships and defences) which are able to fire weapons and take damage.
+public protocol Unit {
+}
